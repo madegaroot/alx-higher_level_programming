f320d83b82e9051a6c57456f01785c23ada08521
madegaroot <giftmadega@gmail.com>
2024-01-30 03:35:37 -0800

    0x07. Python - Test-driven development

---
+++ 0x07-python-test_driven_development/tests/6-main.py
@@ -0,0 +1,7 @@
+guillaume@ubuntu:~/0x07$ cat 6-main.py
+#!/usr/bin/python3
+max_integer = __import__('6-max_integer').max_integer
+
+print(max_integer([1, 2, 3, 4]))
+print(max_integer([1, 3, 4, 2]))
+guillaume@ubuntu:~/0x07$
